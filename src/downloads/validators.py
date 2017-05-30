796ce384937ea4f6b46df9561cc17f843dcba15b
Antonio G. Bolaño <angonzalez@odec.es>
2017-05-30 14:46:09 +0200

    Changed MIME_FILE settings strategy

--- src/downloads/validators.py
+++ src/downloads/validators.py
@@ -12,48 +12,12 @@ import magic
 logger = logging.getLogger(__name__)
 
 
-try:
-    MAGIC_FILE = settings.MAGIC_FILE
-except:
-    MAGIC_FILE = None
-
-"""
-def validate_file_extension(value):
-
-    content_types = settings.DOWNLOADS_ALLOWED_CONTENT_TYPES
-
-    mime_lookup_length = 4096   # 4Kb by default
-
-    if value.file.__class__ == InMemoryUploadedFile:
-        buffer = value.file.file.getvalue()
-        # as the file is already in memory, buffer length
-        # should not exhaust the system
-    else:
-        buffer = value.file.file.read(mime_lookup_length)
-        # but here this may become an issue
-
-    logger.debug('Magic file: %s' % MAGIC_FILE)
-
-    file_magic = magic.Magic(mime=True, magic_file=MAGIC_FILE)
-    mime = file_magic.from_buffer(buffer)
-    logger.debug('Detected MIME: %s' % mime)
-
-    content_type = value.file.content_type
-
-    # Prefere mime-type instead mime-type from http header
-    if content_type != mime:
-        content_type = mime
-
-    if content_type not in content_types:
-        raise ValidationError(
-            _('Files of type %(type)s are not supported.') % {
-                'type': content_type}
-        )
-"""
-
+MAGIC_FILE = None if not hasattr(
+    settings, 'MAGIC_FILE') else settings.MAGIC_FILE
 
 ALLOWED_TYPES = ['application/pdf'] if not hasattr(
-    settings, 'DOWNLOADS_ALLOWED_CONTENT_TYPES') else settings.DOWNLOADS_ALLOWED_CONTENT_TYPES
+    settings, 'DOWNLOADS_ALLOWED_CONTENT_TYPES'
+) else settings.DOWNLOADS_ALLOWED_CONTENT_TYPES
 
 
 @deconstructible
@@ -84,13 +48,11 @@ class FileMimeValidator(object):
         mime = file_magic.from_buffer(buffer)
         logger.debug('Detected MIME: %s' % mime)
 
-        # content_type = value.file.content_type
+        content_type = value.file.content_type
 
-        # # Prefere mime-type instead mime-type from http header
-        # if content_type != mime:
-        #     content_type = mime
-
-        content_type = mime
+        # Prefere mime-type instead mime-type from http header
+        if content_type != mime:
+            content_type = mime
 
         if content_type not in self.allowed_types:
             raise ValidationError(
